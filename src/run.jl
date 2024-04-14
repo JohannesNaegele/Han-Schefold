8418cd75eb06d5cd255d57c5acc0e46a74c04518
Johannes Nägele <johannes-naegele@web.de>
2024-04-15 00:46:54 +0200

    User info

--- src/run.jl
+++ src/run.jl
@@ -12,6 +12,9 @@ include("pairwise.jl")
 include("simultaneous.jl")
 
 results_pairs = pairwise_comparisons(ids, step = 0.01)
+# Beware: This finer grid can take up to several hours to run
+# Make sure to start Julia with several cores if possible,
+# you also need around 16 GB RAM/Swap for this to work
 results_pairs_fine = pairwise_comparisons(ids, step = 0.00001, save_q = false)
 results_simul = simultaneous_comparisons(ids)
 jldsave("results.jld2"; results_pairs, results_pairs_fine, results_simul)

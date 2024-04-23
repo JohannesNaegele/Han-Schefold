ec2ec87badb64abd1e3fa336d08a200f94dfbd59
Johannes Nägele <johannes-naegele@web.de>
2024-04-23 19:53:49 +0200

    First concept to keep track of envelope

--- src/vfz.jl
+++ src/vfz.jl
@@ -1,31 +1,40 @@
-struct Envelope{T}
-    technologies::Set{Vector{T}}
-    tech_dict::Dict{T, Vector{T}}
-    reverse::Dict{Vector{T}, Set{T}}
-    Envelope(tech_dict::Dict{T, Vector{T}}) where T = new{T}(
-        Set(first(tech_dict).second), # get initial technology
-        tech_dict, # get initial mapping
-        Dict(v => Set(findall(==(v), tech_dict)) for (_, v) in tech_dict)
+# TODO: ensure that the techs have all the same length
+struct Envelope{T, R}
+    ids::Dict{Int64, Vector{T}} # map technologies to ids
+    tech_dict::Dict{R, Int64} # map profit rates to ids
+    reverse::Dict{Int64, Set{R}} # map ids to profit rates
+    Envelope(profit_rates, initial_tech) = new{Int64, eltype(profit_rates)}(
+        Dict(1 => initial_tech),
+        Dict(profit_rates .=> 1),
+        Dict(1 => Set(profit_rates))
+        # Dict(v => Set(findall(==(v), tech_dict)) for (_, v) in tech_dict)
     )
 end
 
 # initial
-envelope = Envelope{Float64}(Dict(r => initial_technology for r in profit_rates))
+initial_technology = collect(1:33)
+profit_rates = 0:0.1:1
+envelope = Envelope(profit_rates, initial_technology)
 
-# update
+# Update the envelope with a new technology
 function update_envelope(envelope, r, technology)
-    old_technology = envelope.tech_dict[r]
-    if technology in envelope.reverse
-        push!(envelope.reverse[technology], r)
-        # keep technology in reverse dict cause why not
-        pop!(envelope.old_technology, r)
-    else
-        envelope.tech_dict[r] = technology
-        push!(envelope.reverse, technology => Set([r]))
-        # keep technology in reverse dict cause why not
-        pop!(envelope.old_technology, r)
+    old_id = envelope.tech_dict[r]
+    new_id = findfirst(==(technology), envelope.ids)
+    @assert old_id != new_id "You have to update with a different technology."
+    println(new_id)
+    if isnothing(new_id)
+        new_id = length(envelope.ids) + 1
+        envelope.ids[new_id] = technology
+        envelope.reverse[new_id] = Set([r])
     end
+    envelope.tech_dict[r] = new_id
+    push!(envelope.reverse[new_id], r)
+    # Remove the profit rate from the reverse dict
+    println(old_id, new_id)
+    pop!(envelope.reverse[old_id], r)
 end
+
+update_envelope(envelope, 0.0, [1, 2])
 
 """ Compute the envelope for the book-of-blueprints A with the VFZ algorithm.
 
